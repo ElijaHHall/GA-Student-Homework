78560caee9efe890503e3c67f59c2867cbd46b70
Lillian Chernin <l.smith1654@gmail.com>
2017-12-05 21:40:07 -0600

    Commit 9 - Created loops.js file

---
+++ Week_One/resume_page/loops.js
@@ -0,0 +1,34 @@
+for (let i = 0; i <= 99; i++) {
+  console.log(i);
+}
+
+for (let j = 99; j >= 0; j--) {
+  console.log(j);
+}
+
+//My code below assumes that 0 is not an even number
+for (let k = 0; k <= 98; k++) {
+  if (k % 2 === 0 && k !== 0) {
+    console.log(k);
+  }
+}
+
+for (let m = 98; m >= 0; m--) {
+  if (m % 2 === 0 && m !== 0) {
+    console.log(m);
+  }
+}
+
+for (let l = 49; l <= 72; l++) {
+  console.log(l);
+}
+
+for (let n = 81; n >= 26; n--) {
+  console.log(n);
+}
+
+for (let p = 3; p <= 90; p++) {
+  if (p % 3 === 0) {
+    console.log(p);
+  }
+}
