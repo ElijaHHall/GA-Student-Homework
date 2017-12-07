4c67186304cdd35b10292cbeea4840bd57477cac
Lillian Chernin <l.smith1654@gmail.com>
2017-12-06 19:10:08 -0600

    Commit 4 - Fizz Buzz answered

--- Week_One/Wednesday/answers.js
+++ Week_One/Wednesday/answers.js
@@ -10,16 +10,18 @@ for (let j = 0; j <= 200; j++) {
   }
 }
 
-//Answers for Excited Kittn:
+
+//Answers for Excited Kitten:
 
 // 1.
+
 const message = "Love me, pet me! HSSSSSS!";
 
 for (let k = 0; k < 20; k++) {
   console.log(message);
 }
 
-//2.
+// 2.
 
 const evenMessage1 = "...human...why you taking pictures of me?...";
 const evenMessage2 = "...the catnip made me do it...";
@@ -36,5 +38,20 @@ for (let m = 1; m <= 20; m++) {
     console.log(getRandomMessage(arrayOfEvenMessages));
   } else {
     console.log(message);
+  }
+}
+
+
+//Answer for Fizz Buzz:
+
+for (let n = 1; n <= 100; n++) {
+  if (n % 5 === 0 && n % 3 === 0) {
+    console.log("FizzBuzz");
+  } else if (n % 3 === 0) {
+    console.log("Fizz");
+  } else if (n % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(n);
   }
 }
