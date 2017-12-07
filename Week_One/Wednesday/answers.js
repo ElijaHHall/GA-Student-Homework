28c2264edbaaa2a40301e132e414b8b471a091f4
Lillian Chernin <l.smith1654@gmail.com>
2017-12-06 18:24:09 -0600

    Commit 2 - Get Even answered

--- Week_One/Wednesday/answers.js
+++ Week_One/Wednesday/answers.js
@@ -1,4 +1,11 @@
-//Answers for Easy Going
+//Answers for Easy Going:
 for (let i = 1; i <= 20; i++) {
   console.log(i);
+}
+
+//Answers for Get Even:
+for (let j = 0; j <= 200; j++) {
+  if (j !== 0 && j % 2 === 0) {
+    console.log(j);
+  }
 }
