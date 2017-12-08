fa150cc5275d07838754d5511690455076397822
Lillian Chernin <l.smith1654@gmail.com>
2017-12-07 19:10:27 -0600

    Commit 5 - Sum Array

--- Week_One/Thursday/solutions.js
+++ Week_One/Thursday/solutions.js
@@ -47,3 +47,17 @@ const calculateSide = (sideA, sideB) => {
 }
 
 console.log(calculateSide(8, 6));
+
+
+// Answer for Sum Array :
+
+const sumArray = (array) => {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i];
+  }
+  return sum;
+ }
+
+ console.log(sumArray([1, 2, 3, 4, 5, 6]));
+ 
