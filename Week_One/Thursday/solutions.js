c2e8b0e46e511f92f2e79b30f5712317b62bd527
Lillian Chernin <l.smith1654@gmail.com>
2017-12-07 18:54:27 -0600

    Commit 1 - Verbal questions

---
+++ Week_One/Thursday/solutions.js
@@ -0,0 +1,13 @@
+// Question: What is the difference between a parameter and an argument?
+
+// Answer: An argument is what you pass to a function (in the parentheses) when you invoke it.
+// A parameter is a variable that is the placeholder for the argument (that is provided when the
+// function is invoked) while you are defining the function.  So you use parameters
+// when you define a function and you use arguments when you invoke the function.
+
+// Question: Within a function, what is the difference between return and console.log?
+
+// Answer: A return will stop the execution of the function.  A console.log within
+// a function does not stop the execution and the value within the console.log is
+// printed in the console but it does not go/is not saved anywhere else.  When you
+// use return you make the function return a value that can be used in other processes, functions, etc.
